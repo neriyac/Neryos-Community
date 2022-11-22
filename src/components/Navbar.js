088dc67603d669567c4120be3e07bf33ec143a69
Neryos <brhv0000@gmail.com>
2022-11-22 09:48:29 +0200

    Added Signup component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,10 @@ import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
   return (
-    <div>
+    <div className='navbar'>
       <ul>
         <li className="logo">
-            <img
-                src="Temple"
-                alt="temple logo"
-            />
+            <img src={Temple} alt="temple logo"/>
             <span>Neryos Community</span>
         </li>
         <li><Link to="/login">Login</Link></li>
