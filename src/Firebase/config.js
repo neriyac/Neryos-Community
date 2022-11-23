0e2b7f48d06abbe4e34969db2183fa77190bd21a
Neryos <brhv0000@gmail.com>
2022-11-23 12:38:48 +0200

    Online users bar

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "AIzaSyA2xQuVt_YznXS-aeHKv9A89i9s4j4Vr60",
@@ -18,8 +19,9 @@ const firebaseConfig = {
   // init services
   const projectFirestore = firebase.firestore()
   const projectAuth = firebase.auth()
+  const projectStorage = firebase.storage()
 
   // timestamp
   const timestamp = firebase.firestore.Timestamp
 
-  export { projectFirestore, projectAuth, timestamp }
+  export { projectFirestore, projectAuth, projectStorage, timestamp }
