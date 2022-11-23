9837f7397ff9576700b637ae7efb954699857755
Neryos <brhv0000@gmail.com>
2022-11-23 22:21:49 +0200

    Updated ProjectSummary

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useDocument } from '../../hooks/useDocument'
+import ProjectSummary from './ProjectSummary'
+
 //styles
 import './Project.css'
 
@@ -16,7 +18,7 @@ export default function Project() {
 
   return (
     <div className='project-detailes'>
-      <h1>{document.name}</h1>
+      <ProjectSummary project={document}/>
     </div>
   )
 }
