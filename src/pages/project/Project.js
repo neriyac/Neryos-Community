d14da187099959974f40dfc41927059ea93c26cb
Neryos <brhv0000@gmail.com>
2022-11-23 22:00:51 +0200

    useDocument hook

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,10 +1,22 @@
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
 //styles
 import './Project.css'
 
 export default function Project() {
+  const { id } = useParams() // this "id" comes from (Route path="/projects/:id") we choose in App.
+  const { document, error } = useDocument('projects', id)
+
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>
+  }
+
   return (
-    <div>
-      Project
+    <div className='project-detailes'>
+      <h1>{document.name}</h1>
     </div>
   )
 }
