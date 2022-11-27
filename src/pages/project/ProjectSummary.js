b2c7caddefee9b0024e2a08e930b9a04dbef934b
Neryos <brhv0000@gmail.com>
2022-11-27 16:27:22 +0200

    Complete project option

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,10 +1,25 @@
+import { useHistory } from 'react-router-dom'
 import Avatar from '../../components/Avatar'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useFirestore } from '../../hooks/useFirestore'
+
+
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument} = useFirestore('projects')
+  const { user } = useAuthContext()
+  const history = useHistory()
+
+  const handleClick = (e) => {
+    deleteDocument(project.id)
+    history.push("/")
+  }
+  
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>Created by {project.createdBy.displayName}</p>
         <p className="due-date">
             Project due by {project.dueDate.toDate().toDateString()}
         </p>
@@ -18,6 +33,9 @@ export default function ProjectSummary({ project }) {
             ))}
         </div>
       </div>
+              {user.uid === project.createdBy.id && (
+          <button className="btn" onClick={handleClick}>Mark as complete</button>
+        )}
     </div>
   )
 }
