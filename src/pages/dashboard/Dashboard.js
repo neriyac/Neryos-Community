2b613f3fe48460d9eee6d451d991e240e43dad22
Neryos <brhv0000@gmail.com>
2022-11-27 16:50:07 +0200

    Filter first setup

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { useCollection } from '../../hooks/useCollection'
 
 //styles
 import './Dashboard.css'
+import ProjectFilter from './ProjectFilter'
 
 export default function Dashboard() {
   const { documents, error } = useCollection('projects')
@@ -10,6 +11,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p> }
+      {documents && <ProjectFilter/>}
       {documents && <ProjectList projects={documents}/>}
     </div>
   )
